8f0fef97e1b73f94cb6ca5b9d93b1bd57c492429
Bufka2011 <bufka.bulanov@gmail.com>
2026-09-18 16:24:26 -0600

    klog: drop cursor show/hide; fill then write avoids white [ at pos 1

--- boot/kernel/main.lua
+++ boot/kernel/main.lua
@@ -407,13 +407,11 @@ function K.klog(msg)
   klogPush(msg)
   local g = gpu0()
   if g then
-    ttyHideCursor()
-    g.setForeground(0xFFAA00)
     local w = g.getResolution()
     g.fill(1, 1, w or 80, 1, " ")
+    g.setForeground(0xFFAA00)
     g.set(1, 1, "[freax] " .. tostring(msg))
     g.setForeground(0xFFFFFF)
-    ttyShowCursor()
   end
 end
 
